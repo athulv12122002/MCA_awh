b5a4b0bac5bbb9dc2fcbba27c0e98cceac227199
athulv12122002 <57036491+athulv12122002@users.noreply.github.com>
2023-11-07 12:45:20 +0530

    Create greatest.py
    Find the greatest of 3 numbers entered.

---
+++ S1/Python/greatest.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the First Element"))
+b=int(input("Enter the Second Element"))
+c=int(input("Enter the Third Element"))
+
+if a>b :
+    if a>c :
+        print("a is the greatest ")
+    else :
+        print("c is the greatest ")
+else :
+    if b>c :
+        print("b is the greatest ")
+    else :
+        print("c is the greatest ")        
