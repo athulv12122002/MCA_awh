fd01e7030ca7776f0ca32085852fb99cbcafa035
athulv12122002 <57036491+athulv12122002@users.noreply.github.com>
2024-03-01 15:41:03 +0530

    Create firstIndex.java

---
+++ s2/Java/firstIndex.java
@@ -0,0 +1,11 @@
+class First{
+        public static void main(String args[])
+                {
+                        String[] arr1={"10","20","30","40"};
+                        System.out.println(arr1[0]);
+
+                }
+
+
+
+        }
