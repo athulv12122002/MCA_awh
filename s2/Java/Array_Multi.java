c5c7067c2f7c4a4d27e4625f3e4e7ce75aca1c14
athulv12122002 <57036491+athulv12122002@users.noreply.github.com>
2024-03-06 15:24:07 +0530

    Create Array_Multi.java

---
+++ s2/Java/Array_Multi.java
@@ -0,0 +1,13 @@
+class Multi{
+        public static void main(String args[])
+                {
+                System.out.println("---MULTIPLICATION TABLE---");
+                        int [] arr;
+                        arr= new int[11];
+                        for(int i=1;i<11;i++){
+                                arr[i]=i*5;
+                                System.out.println(i+"*5="+arr[i]);
+                        }
+
+                }
+        }
