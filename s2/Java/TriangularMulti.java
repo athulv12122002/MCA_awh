fcc8422e228a02e189808b3389f727133c744246
athulv12122002 <57036491+athulv12122002@users.noreply.github.com>
2024-07-16 21:55:17 +0530

    Create TriangularMulti.java

--- s2/Java/TriangularMulti.java
+++ s2/Java/TriangularMulti.java
@@ -1,19 +1,16 @@
 import java.util.Scanner;
-
-public class TriangularMulti {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows for the triangular multiplication table:");
-        int rows = sc.nextInt();
-
-        for (int i = 1; i <= rows; i++) {
-            System.out.print("0 ");
-            for (int j = 1; j <= i; j++) {
-                System.out.print(i * j + " ");
-            }
-            System.out.println();
+public class TriMul{
+        public static void main(String[]args){
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter the Number of Rows for the Triangular Multiplication Table: ");
+                int rows = sc.nextInt();
+                System.out.println("The Triangular Multiplication table of " +rows +" is: ");
+                for(int i=0;i<rows;i++){
+                        System.out.print("");
+                        for(int j=0;j<=i;j++){
+                                System.out.print(i * j + " ");
+                        }
+                        System.out.println();
+                }
         }
-
-        sc.close();
-    }
 }
